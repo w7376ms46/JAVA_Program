63f34439e6beb67111b1a4623857e89d21dd4075
w7376ms46 <w7376ms46@hotmail.com>
2013-11-06 18:04:37 +0800

    ADSFBASDFADASF
    ASDFASDFASD

--- J5_6_1/J5_6_1.java
+++ J5_6_1/J5_6_1.java
@@ -5,7 +5,7 @@ public class J5_6_1 {
 		double d1=12345.765;
 		NumberFormat nf1=NumberFormat.getInstance();
 		System.out.println(nf1.format(d1));
-		System.out.println(NumberFormat.getCurrencyInstance().format(d1));
+		System.out.println(NASDFASDFASDFAmat.getCurrencyInstance().format(d1));
 		
 	}
 }
